a1b9b4610861742adb6a40bac7ffc9a3ba6de39e
poojatgit <ptummala@terpmail.umd.edu>
2025-04-16 23:04:49 -0400

    Docstrings for MediaItem Class
    Driver: Pooja Navigator: Andrea

--- media_finder.py
+++ media_finder.py
@@ -1,3 +1,37 @@
-print("Hello")
-print("Bye")
-print("testing pooja edits")
+
+
+class MediaItem:
+    """
+    A class representing a media item such as a movie or TV show and updating the status of them.
+
+    Attributes:
+        title (str): The title of the media.
+        genre (str): The genre of the media.
+        status (str): The completion status, either 'not completed' or 'completed'.
+        platform (str): The streaming service where the media is available.
+    """
+
+    def __init__(self, title, genre, platform):
+        pass
+
+    def mark_completed(self):
+        """
+        Marks the media item as completed or not completed
+        """
+        pass
+
+    def where_to_watch(self):
+        """
+        finds the platform where the media is and returns where it can be watched
+        """
+        pass
+
+    def __repr__(self):
+        """
+        show the user a readable verision of the attributes 
+        """
+    
+    def test_media(self):
+        """
+        this will test the class and make sure that it is running smoothly 
+        """
